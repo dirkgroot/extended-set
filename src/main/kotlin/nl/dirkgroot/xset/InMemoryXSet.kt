ee252c11bd03220525346ccfbb4547ee8478e607
Dirk Groot <d.groot@avisi.nl>
2021-12-23 19:28:57 +0100

    Move docstring to interface

--- src/main/kotlin/nl/dirkgroot/xset/InMemoryXSet.kt
+++ src/main/kotlin/nl/dirkgroot/xset/InMemoryXSet.kt
@@ -14,11 +14,5 @@ class InMemoryXSet<S, E> : XSet<S, E> {
 
     override fun contains(scope: S, element: E) = map[scope]?.any { it == element } ?: false
 
-    /**
-     * Element projection. Returns the value at the given scope if, and only if,
-     * the set contains exactly 1 element at the given scope. Returns `null` otherwise.
-     *
-     * @param scope the scope to project.
-     */
     override fun at(scope: S) = map[scope]?.singleOrNull()
 }
--- src/main/kotlin/nl/dirkgroot/xset/XSet.kt
+++ src/main/kotlin/nl/dirkgroot/xset/XSet.kt
@@ -6,5 +6,12 @@ interface XSet<S, E> {
     fun add(scope: S, element: E)
     fun isEmpty(): Boolean
     fun contains(scope: S, element: E): Boolean
+
+    /**
+     * Element projection. Returns the value at the given scope if, and only if,
+     * the set contains exactly 1 element at the given scope. Returns `null` otherwise.
+     *
+     * @param scope the scope to project.
+     */
     fun at(scope: S): E?
 }
